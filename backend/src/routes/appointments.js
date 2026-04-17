19243e66a94698dd8c6e6b0e98cc36296df9183a
Safiyyah Saloojee <165449835+safs11111@users.noreply.github.com>
2026-04-17 23:16:51 +0200

    Update appointments.js

--- backend/src/routes/appointments.js
+++ backend/src/routes/appointments.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const appointmentController = require('../controllers/appointmentController');
 
-// CREATE BOOKING
+// Create booking
 router.post('/', appointmentController.createBooking);
+
+// Get my appointments
+router.get('/my', appointmentController.getMyAppointments);
 
 module.exports = router;

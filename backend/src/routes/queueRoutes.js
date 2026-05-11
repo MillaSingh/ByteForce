a26e6ec537325a50f8813dc4924ff91286b32c2b
Milla Singh <2815918@students.wits.ac.za>
2026-05-11 10:26:44 +0200

    added comments

--- backend/src/routes/queueRoutes.js
+++ backend/src/routes/queueRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 const { requireAuth } = require("./authRoutes");
 const { getMyQueueEntry } = require("../controllers/queueController");
-
+// POST route for getting the current user's queue information
 router.post("/my", requireAuth, getMyQueueEntry);
 module.exports = router;

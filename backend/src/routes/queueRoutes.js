4a85585a1a0cc4903d01e96eda5da6575d973294
Safiyyah Saloojee <165449835+safs11111@users.noreply.github.com>
2026-05-13 23:03:06 +0200

    add check in button functionality

--- backend/src/routes/queueRoutes.js
+++ backend/src/routes/queueRoutes.js
@@ -2,7 +2,20 @@ const express = require("express");
 const router = express.Router();
 
 const { requireAuth } = require("./authRoutes");
-const { getMyQueueEntry } = require("../controllers/queueController");
-// POST route for getting the current user's queue information
+
+const {
+  getMyQueueEntry,
+  checkIn
+} = require("../controllers/queueController");
+
+// GET MY QUEUE ENTRY
 router.post("/my", requireAuth, getMyQueueEntry);
+
+// CHECK IN TO QUEUE
+router.post(
+  "/checkin/:appointment_id",
+  requireAuth,
+  checkIn
+);
+
 module.exports = router;

fdfbc161190dd177d23cd1a50868328fbfe3d1e6
Safiyyah Saloojee <165449835+safs11111@users.noreply.github.com>
2026-05-15 15:59:58 +0200

    Update myAppointments.js

--- frontend/js/myAppointments.js
+++ frontend/js/myAppointments.js
@@ -207,20 +207,20 @@ function loadAppointmentsByData(data) {
             isUpcoming
 
               ? `
-                <button
+                <button>
                   class="reschedule-btn"
                   data-id="${app.appointment_id || app.appointmentId}"
                   data-clinic="${app.clinic_id}">
                   Reschedule
                 </button>
 
-                <button
+                <button>
                   class="cancel-btn"
                   data-id="${app.appointment_id || app.appointmentId}"
                   Cancel
                 </button>
 
-                <button
+                <button>
                 class="checkin-btn"
                 data-id="${app.appointment_id || app.appointmentId}"
                 Check In
